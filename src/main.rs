0ad137553ea159d81bb5b248a5fda2a6074948ed
Leopold Johannes Meinel <leo@meinel.dev>
2022-05-18 06:32:23 +0200

    Optimize imports
    Signed-off-by: Leopold Johannes Meinel <leo@meinel.dev>

--- src/main.rs
+++ src/main.rs
@@ -15,40 +15,41 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see https://github.com/TamrielNetwork/project/blob/main/LICENSE
  */
-use colored::*;
-use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+use colored::Colorize;
+use rand::Rng;
+
 fn main() {
-	println!("Guess the number!");
-	let secret_number = rand::thread_rng().gen_range(0..101);
-	println!("The secret number is: {}", secret_number);
-	loop {
-		println!("Please input your guess!");
-		let mut guess = String::new();
-		match io::stdin().read_line(&mut guess) {
-			Ok(string) => string,
-			Err(_) => {
-				println!("{}", "WARNING: Failed to read line!".bright_red());
-				continue;
-			}
-		};
-		let guess: u32 = match guess.trim().parse() {
-			Ok(num) => num,
-			Err(_) => {
-				println!("{}", "WARNING: Please type a number!".bright_red());
-				continue;
-			}
-		};
-		println!("You guessed: {}", guess);
-		match guess.cmp(&secret_number) {
-			Ordering::Less => println!("{}", "Too small!".yellow()),
-			Ordering::Equal => {
-				println!("{}", "Correct!".green());
-				break;
-			}
-			Ordering::Greater => println!("{}", "Too big!".yellow()),
-		}
-	}
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(0..101);
+    println!("The secret number is: {}", secret_number);
+    loop {
+        println!("Please input your guess!");
+        let mut guess = String::new();
+        match io::stdin().read_line(&mut guess) {
+            Ok(string) => string,
+            Err(_) => {
+                println!("{}", "WARNING: Failed to read line!".bright_red());
+                continue;
+            }
+        };
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{}", "WARNING: Please type a number!".bright_red());
+                continue;
+            }
+        };
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small!".yellow()),
+            Ordering::Equal => {
+                println!("{}", "Correct!".green());
+                break;
+            }
+            Ordering::Greater => println!("{}", "Too big!".yellow()),
+        }
+    }
 }
